7b95db56218f51c8cb35b62aa5096fe1bb29411b
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2023-11-27 02:02:59 +0900

    update

--- scripts/settings.py
+++ scripts/settings.py
@@ -5,21 +5,17 @@ import os
 from modules import shared
 from modules import script_callbacks
 
-# def on_ui_settings():
-#         section = ("sampler-params", "Sampler parameters")
-#         shared.opts.add_option(
-#                 "restart_steps", 
-#                 OptionInfo(
-#                         1.0,
-#                         "restart_steps for Multi Restart",
-#                         gr.Slider,
-#                         {"minimum": 6.0, "maximum": 100.0, "step": 1.0},
-#                         infotext='Eta'
-#                         ).info("Values of restart_steps for Multi Restart"),
-#         )
-
 def on_ui_settings():
-    section = ('multi_restart', "Multi Restart")
-    shared.opts.add_option("restart_steps", OptionInfo(6, label="restart_steps for Multi Restart", component=gr.Slider, component_args={"minimum": 0, "maximum": 100, "step": 1}, section=section))
+    section = ("sampler-params", "Sampler parameters")
+    shared.opts.add_option(
+        "restart_steps", 
+        shared.OptionInfo(
+            6, 
+            label="restart_steps for Multi Restart", 
+            component=gr.Slider, 
+            component_args={"minimum": 0, "maximum": 100, "step": 1},
+            section=section
+        )
+    )
 
 script_callbacks.on_ui_settings(on_ui_settings)
