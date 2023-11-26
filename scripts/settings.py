ba43d1434a42b9d4d5943e366aaaee6f8e34dd42
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2023-11-27 01:53:33 +0900

    update

--- scripts/settings.py
+++ scripts/settings.py
@@ -19,15 +19,16 @@ from modules import script_callbacks
 #         )
 
 def on_ui_settings():
-        section = ('multi_restart', "Multi Restart")
-        shared.opts.add_option(
-                "restart_steps",
-                shared.OptionInfo
-                        6,
-                        label="restart_steps for Multi Restart",
-                        component=gr.Slider,
-                        component_args={"minimum": 0, "maximum": 100, "step": 1},
-                        section=section,
-                )
+    section = ('multi_restart', "Multi Restart")
+    shared.opts.add_option(
+        "restart_steps",
+        shared.OptionInfo(
+            6,
+            label="restart_steps for Multi Restart",
+            component=gr.Slider,
+            component_args={"minimum": 0, "maximum": 100, "step": 1},
+            section=section,
+        )
+    )
 
 script_callbacks.on_ui_settings(on_ui_settings)
